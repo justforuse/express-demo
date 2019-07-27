5c5ae4d0d7f065ba49f9706f5f38070b2d28eee1
justforuse <allenforuse@gmail.com>
2019-07-27 17:12:26 +0800

    fix: github alert

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,32 @@
-$(function () {
-    $("form").on("submit", function () {
-        console.log($("form"));
-        $.ajax({
-            method: "POST",
-            url: "user",
-            data: {
-                name: $("input[name='name'").val(),
-                age: $("input[name='age'").val(),
-                gender: $("input[name='gender'").val(),
-            },
-            success: function () {
-                window.location.reload();
-            }
-        })
-
-        return false;
+$(function() {
+  $('form').on('submit', function() {
+    console.log($('form'))
+    $.ajax({
+      method: 'POST',
+      url: 'user',
+      data: {
+        name: $("input[name='name'").val(),
+        age: $("input[name='age'").val(),
+        gender: $("input[name='gender'").val()
+      },
+      success: function() {
+        window.location.reload()
+      }
     })
 
-    $(".delete-btn").on("click", function () {
-        console.log($(this).attr("user-name"));
-        var userName = $(this).attr("user-name");
-        $.ajax({
-            method: "DELETE",
-            url: `/user/${userName}`,
-            success: function () {
-                console.log("delete success");
-                window.location.reload();
-            }
-        })
-    })
+    return false
+  })
 
+  $('.delete-btn').on('click', function() {
+    console.log($(this).attr('user-name'))
+    var userName = $(this).attr('user-name')
+    $.ajax({
+      method: 'DELETE',
+      url: `/user/${userName}`,
+      success: function() {
+        console.log('delete success')
+        window.location.reload()
+      }
+    })
+  })
 })
-
